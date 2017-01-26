135dbbf3d5f049e49df741420f10888ce0521239
Cleber Santos <e@binho.net>
2017-01-26 13:29:05 -0200

    A few changes in colors and paddings

--- site.go
+++ site.go
@@ -19,24 +19,34 @@ func main() {
 		Layout: "layout",
 	}))
 
-	// Tela inicial
 	m.Get("/", func(r render.Render) {
 		r.HTML(200, "index", nil)
 	})
 
-	// Sobre
 	m.Get("/about", func(r render.Render) {
 		r.HTML(200, "about", nil)
 	})
 
-	// Contato
-	// m.Get("/contact", func(r render.Render) {
-	// 	r.HTML(200, "contact", nil)
-	// })
-
 	m.Get("/work", func(r render.Render) {
 		r.HTML(200, "work", nil)
 	})
+
+	m.Get("/work/:name", func(r render.Render, params martini.Params, req *http.Request) {
+		pwd, _ := os.Getwd()
+		filename := params["name"]
+		filepath := (pwd + "/templates/work/" + filename + ".tmpl")
+
+		if _, err := os.Stat(filepath); err == nil {
+			r.HTML(200, ("work/" + filename), nil)
+		} else {
+			htmlParams := map[string]interface{}{"project_name": params["name"]}
+			r.HTML(404, "not-found", htmlParams)
+		}
+	})
+
+	// m.Get("/contact", func(r render.Render) {
+	// 	r.HTML(200, "contact", nil)
+	// })
 
 	// m.Post("/contact/send", func(r render.Render, req *http.Request) {
 	// 	e := email.NewEmail()
@@ -56,20 +66,6 @@ func main() {
 	// 	htmlParams := map[string]interface{}{"success": true}
 	// 	r.HTML(200, "contact", htmlParams)
 	// })
-
-	// Projetos
-	m.Get("/work/:name", func(r render.Render, params martini.Params, req *http.Request) {
-		pwd, _ := os.Getwd()
-		filename := params["name"]
-		filepath := (pwd + "/templates/work/" + filename + ".tmpl")
-
-		if _, err := os.Stat(filepath); err == nil {
-			r.HTML(200, ("work/" + filename), nil)
-		} else {
-			htmlParams := map[string]interface{}{"project_name": params["name"]}
-			r.HTML(404, "not-found", htmlParams)
-		}
-	})
 
 	m.RunOnAddr(":8080")
 }
